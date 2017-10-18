88311b84896c255a70d88d35aa00ba55dfc447fa
Peter Swanson <topazoo42@gmail.com>
2017-10-18 05:40:00 -0700

    Cleaned

--- Graphs/bfs_dfs.cpp
+++ Graphs/bfs_dfs.cpp
@@ -3,10 +3,13 @@
 #include <iterator>
 #include <stack>
 #include <queue>
+#include<limits>
 
 using namespace std;
 
-int matrix[10][10]  = {
+const int num_mats = 2;
+
+const int matrix[10][10]  = {
                     {0,1,0,0,0,0,0,1,0,0},
                     {1,0,1,1,0,0,1,0,0,0},
                     {0,1,0,0,0,0,0,0,0,0},
@@ -19,25 +22,18 @@ int matrix[10][10]  = {
                     {0,0,0,0,0,0,0,1,0,0},
                     };
 
-/* ---------------------------- Debug ---------------------------- */
-
-void print_list(list<int>* adj_list)
-{
-    //Print an adjacency list
-    int num_nodes = sizeof(*matrix)/sizeof(**matrix);
-    list<int>::iterator adj_node;
-
-    for(int node_list = 0; node_list < num_nodes; node_list++)
-    {
-        for(adj_node = adj_list[node_list].begin(); 
-            adj_node != adj_list[node_list].end(); adj_node++)
-            cout << *adj_node << " ";
-
-        cout << endl;
-    }
-}
-
-/* --------------------------------------------------------------- */
+const int matrix2[10][10]  = {    //Disjoint
+                    {0,1,1,0,0,0,0,0,0,0},
+                    {1,0,1,1,0,0,0,0,0,0},
+                    {1,1,0,0,1,0,0,0,0,0},
+                    {0,1,0,0,1,0,0,0,0,0},
+                    {0,0,1,1,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,1,0,0,1},
+                    {0,0,0,0,0,1,0,1,0,0},
+                    {0,0,0,0,0,0,1,0,0,1},
+                    {0,0,0,0,0,0,0,0,0,1},
+                    {0,0,0,0,0,1,0,1,1,0},
+                    };
 
 list<int>* matrix_to_list(int num_nodes) {
 
@@ -53,35 +49,49 @@ list<int>* matrix_to_list(int num_nodes) {
     return adj_list;
 }
 
-void run_bfs(list<int>* adj_list, int num_nodes)
+list<int>* matrix2_to_list(int num_nodes) {
+
+    //Dynamically create an array containing a list for each node.
+    list<int>* adj_list = new list<int>[num_nodes]; 
+
+    //Populate the lists in the array from the matrix
+    for(int node_list = 0; node_list < num_nodes; node_list++)
+        for(int node = 0; node < num_nodes; node++ )
+            if(matrix2[node_list][node] == 1)
+                adj_list[node_list].push_back(node);
+
+    return adj_list;
+}
+
+void run_bfs(list<int>* adj_list, int num_nodes, int start_node)
 {
     queue<int> nodes_to_visit;
     list<int>::iterator adj_node;
-    int visited_node_count = 0;
     bool* visited_nodes = new bool[num_nodes]; 
 
     cout << "BFS Result: "; 
 
     //Add first node to queue
-    nodes_to_visit.push(0);
+    nodes_to_visit.push(start_node);
+    visited_nodes[start_node] = 1;
 
     //Until all nodes are visited:
-    while(visited_node_count < num_nodes)
+    while(! nodes_to_visit.empty())
     {
         //Get the next node from the queue
         int node_to_visit = nodes_to_visit.front();
+        
+        //Remove visited node and mark as visited
+        nodes_to_visit.pop();
         
         //Add all unvisited adjacent nodes to the queue
         for(adj_node = adj_list[node_to_visit].begin(); 
             adj_node != adj_list[node_to_visit].end(); adj_node++)
             if(visited_nodes[*adj_node] == 0)
+            {
                 nodes_to_visit.push(*adj_node);
-    
-        //Remove visited node and mark as visited
-        nodes_to_visit.pop();
-        visited_nodes[node_to_visit] = 1;
-        visited_node_count++;
-
+                visited_nodes[*adj_node] = 1;
+            }
         //Print visited node
         cout << node_to_visit << " ";
     }
@@ -89,25 +99,109 @@ void run_bfs(list<int>* adj_list, int num_nodes)
     cout << endl;
 }
 
-void run_dfs(list<int>* adj_list, int num_nodes)
+void run_dfs(list<int>* adj_list, int num_nodes, 
+                bool* visited_nodes, int start_node)
 {
-    cout << "Running DFS" << endl;
+    list<int>::iterator adj_node;
+
+    //If first recursive step, create list of visited nodes
+    if(! visited_nodes)
+    {
+        visited_nodes = new bool[num_nodes];
+        cout << "DFS Result: "; 
+    }
+
+    //Mark node as visited
+    cout << start_node << " ";
+    visited_nodes[start_node] = 1;
+
+    //Recurse on all unvisited children
+    for(adj_node = adj_list[start_node].begin(); 
+        adj_node != adj_list[start_node].end(); adj_node++)
+        if(visited_nodes[*adj_node] == 0)
+            run_dfs(adj_list, num_nodes, visited_nodes, *adj_node);
+
+}
+
+int get_input_matrix()
+{
+    //Matrix to perform search on
+
+    int mat = 0;
+
+    cout << "Choose a Matrix:" << endl;
+
+    for(int i = 1; i <= num_mats; i++)
+        cout << "Matrix " << i << endl;
+
+    cout << ">>> ";
+
+    while(! (cin >> mat) || mat < 1 || mat > num_mats)
+    {
+        cout << "Choose a Matrix:" << endl;
+
+        for(int i = 1; i <= num_mats; i++)
+            cout << "Matrix " << i << endl;
+
+        cout << ">>> ";
+
+        //Clear old input
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    return mat;
+}
+
+int get_input_start(int num_nodes)
+{
+    int start = -1;
+
+    cout << "Choosing a starting node between 0 and " << num_nodes - 1 << endl;
+    cout << ">>> ";
+
+    while(! (cin >> start) || start < 0 || start >= num_nodes)
+    {
+        cout << "Choosing a starting node between 0 and " << num_nodes - 1 << endl;
+        cout << ">>> ";
+
+        //Clear old input
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    return start;
 }
 
 int main() {
 
-    //Create an adjacency list from an adjacency matrix
     list<int>* adj_list;
-    int num_nodes = sizeof(*matrix)/sizeof(**matrix);
-    
-    adj_list =  matrix_to_list(num_nodes);
+    int num_nodes, mat, start;
+
+    //User chooses matrix
+    mat = get_input_matrix();
+
+    //Create an adjacency list from an adjacency matrix
+    if(mat == 1)
+    {
+        num_nodes = sizeof(*matrix)/sizeof(**matrix);
+        adj_list =  matrix_to_list(num_nodes);
+    }
+
+    else if(mat == 2)
+    {
+        num_nodes = sizeof(*matrix2)/sizeof(**matrix2);
+        adj_list =  matrix2_to_list(num_nodes);
+    }    
+
+    start = get_input_start(num_nodes);
 
     //Run a Breadth-First Search of the graph
-    run_bfs(adj_list, num_nodes);
+    run_bfs(adj_list, num_nodes, start);
 
     //Run a Depth-First Search of the graph
-    run_dfs(adj_list, num_nodes);
-
-    //print_list(adj_list);
+    run_dfs(adj_list, num_nodes, NULL, start);
+    
+    cout << endl;
 
 }
